7fcb9ab346ca76101c254a49c936ee4d77e69e3d
KarenElaine Stewart <karenelaine.stewart@yahoo.com>
2022-04-29 15:53:27 -0400

    added logout functionality and installed react-select

--- find-dining/src/components/MenuHeader.tsx
+++ find-dining/src/components/MenuHeader.tsx
@@ -1,22 +1,58 @@
-import "./MenuHeader.css"
-const MenuHeader = () => {
+import "./MenuHeader.css";
+import axios from "axios";
+import { useState } from "react";
+
+interface props {
+  token:string,
+  setToken:React.Dispatch<unknown>
+  setUser:React.Dispatch<unknown>
+}
+
+const MenuHeader = ({ token, setToken, setUser }:props) => {
+  const [error, setError] = useState("");
+  const setLogout = () => {
+    const options = {
+      method: "POST",
+      url: "https://find-dining-panda.herokuapp.com/api/auth/token/logout/",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+      data: {
+        token: `${token}`,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setUser("");
+        setToken("");
+      })
+      .catch((e) => setError("Logout Unsuccessful Try again"));
+  };
+
   return (
     <div className="menuHeader">
-            <img
+      <img
         className="siteLogo"
         src={require("../images/panda.jpeg")}
         alt="This a placeholder"
       ></img>
       <div className="logoutProfile">
-      <img
-        className="profilePic"
-        src={require("../images/panda.jpeg")}
-        alt="This a placeholder"
-      ></img>
-      <a  href="home" className="logoutLink">Logout</a>
+        <img
+          className="profilePic"
+          src={require("../images/panda.jpeg")}
+          alt="This a placeholder"
+        ></img>
+        <div className="error">{error}</div>
+        <a href="home" className="logoutLink" onClick={setLogout}>
+          Logout
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MenuHeader
+export default MenuHeader;
