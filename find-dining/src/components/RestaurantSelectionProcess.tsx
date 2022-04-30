08b8a95a8c0ad32fbd7572506ca8bf837ffa8467
Paul Reece <paulreece2@gmail.com>
2022-04-30 15:32:26 -0400

    swiping start

--- find-dining/src/components/RestaurantSelectionProcess.tsx
+++ find-dining/src/components/RestaurantSelectionProcess.tsx
@@ -5,51 +5,58 @@ import "./RestaurantSelectionProcess.css"
 
 const db = [
     {
-      name: 'Richard Hendricks',
-      url: '../images/panda.jpeg'
-    },
-    {
-      name: 'Erlich Bachman',
-      url: '../images/panda.jpeg'
-    },
-    {
-      name: 'Monica Hall',
-      url: '../images/panda.jpeg'
-    },
-    {
-      name: 'Jared Dunn',
-      url: '../images/panda.jpeg'
-    },
-    {
-      name: 'Dinesh Chugtai',
-      url: '../images/panda.jpeg'
-    }
-  ]
+        name: 'Paul',
+        url: './img/Paul.jpg'
+      },
+      {
+        name: 'Ryan',
+        url: './img/Ryan.jpg'
+      },
 
+      {
+        name: 'Tyler',
+        url: './img/Tyler.png'
+      },
+      {
+        name: 'KE',
+        url: './img/KE.jpg'
+      },
+
+  ]
+  let yes = []
+  let no = []
 const RestaurantSelectionProcess = () => {
-    const characters = db
-    const [lastDirection, setLastDirection] = useState()
-  
-    const swiped = (direction, nameToDelete) => {
+  const characters = db
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
       console.log('removing: ' + nameToDelete)
+      direction === "right" ? (yes.push(nameToDelete)) : (no.push(nameToDelete))
       setLastDirection(direction)
+      
     }
-  
+
     const outOfFrame = (name) => {
       console.log(name + ' left the screen!')
     }
+    console.log(yes)
+    console.log(no)
   return (
-        <div>
+        <div className='cardDeck'>
       <div className='cardContainer'>
-        {characters.map((character) =>
-          <RestaurantCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+        {characters.map((character, index) =>
+          <RestaurantCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} 
+          onCardLeftScreen={() => outOfFrame(character.name)} flickOnSwipe={true} preventSwipe={["up", "down"]}>
             <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
               <h3>{character.name}</h3>
+              {/* <h3>{index}</h3> */}
             </div>
+            
           </RestaurantCard>
         )}
+        
       </div>
-      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+      {lastDirection ? <h2 className='infoText'> {lastDirection==="right" ? ("You said Yes!"): lastDirection === "left" ? ("You said No!"):("You can only swipe left or Right, please swipe again!")}</h2> : <h2 className='infoText' />}
     </div>
   )}
 
