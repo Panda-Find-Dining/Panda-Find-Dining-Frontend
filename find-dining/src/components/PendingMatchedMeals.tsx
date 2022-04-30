a0f8235a74d750e77a05512af887623824383343
KarenElaine Stewart <karenelaine.stewart@yahoo.com>
2022-04-30 15:31:09 -0400

    fixed deploy error

--- find-dining/src/components/PendingMatchedMeals.tsx
+++ find-dining/src/components/PendingMatchedMeals.tsx
@@ -12,24 +12,7 @@ const PendingMatchedMeals = () => {
     </button>
   </div>
   <div className="dropdown-menu" id="dropdown-menu" role="menu">
-    <div className="dropdown-content">
-      <a href="#" className="dropdown-item">
-        Dropdown item
-      </a>
-      <a className="dropdown-item">
-        Other dropdown item
-      </a>
-      <a href="#" className="dropdown-item is-active">
-        Active dropdown item
-      </a>
-      <a href="#" className="dropdown-item">
-        Other dropdown item
-      </a>
-      <hr className="dropdown-divider"/>
-      <a href="#" className="dropdown-item">
-        With a divider
-      </a>
-    </div>
+    
   </div>
 </div>
   )
