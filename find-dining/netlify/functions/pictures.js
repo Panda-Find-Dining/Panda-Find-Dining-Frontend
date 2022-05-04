63f86d715715bdc9c75048bf34c13310c988671e
Paul Reece <paulreece2@gmail.com>
2022-05-03 21:32:18 -0400

    netlify function

--- find-dining/netlify/functions/pictures.js
+++ find-dining/netlify/functions/pictures.js
@@ -4,8 +4,10 @@ exports.handler = async function (event, context) {
   console.log(event);
   console.log(context);
   try {
-    const { id } = event.queryStringParameters;
-    const response = await axios.get(`https://www.google.com/`);
+    // const { id } = event.queryStringParameters;
+    const response = await axios.get(
+      "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference="
+    );
     return {
       statusCode: 200,
       body: JSON.stringify({ title: response.data.title }),
--- find-dining/src/components/RestaurantSelectionProcess.tsx
+++ find-dining/src/components/RestaurantSelectionProcess.tsx
@@ -190,7 +190,7 @@ console.log(restPk)
 // }, []);
 useEffect(() => {
 fetch(`/.netlify/functions/pictures`)
-.then(res => { console.log(res.data) })
+.then(res => { console.log(res.json()) })
 },[]);
 
 const google1 = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference="
