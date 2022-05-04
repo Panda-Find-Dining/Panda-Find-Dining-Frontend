1ba48e62cfde3f135789a488a83a0f22cadcea75
KarenElaine Stewart <karenelaine.stewart@yahoo.com>
2022-05-03 21:30:51 -0400

    login styled and updated menubar logo

--- find-dining/src/components/OnBoard.tsx
+++ find-dining/src/components/OnBoard.tsx
@@ -25,97 +25,106 @@ const Span = styled.span`
   color: #da0063;
   font: Lato;
   font-weight: bold;
-`
+`;
 const PandaGreeting = styled.div`
-// position: relative;
-// text-align: center;
-// color: white;
-`
+  // position: relative;
+  // text-align: center;
+  // color: white;
+`;
 const Blurb = styled.div`
-text-align: center;
-margin: 10px;
-padding: 25px;
-`
+  text-align: center;
+  margin: 10px;
+  padding: 25px;
+`;
 const Container = styled.div`
   padding: 25px;
   display: flex;
   flex-direction: column;
-`
+`;
 const Exit = styled.div`
-  text-align: right
-`
+  text-align: right;
+`;
 const PandaTalk = styled.div`
-text-align: right;
-padding-top: 10px;
-padding: 25px;
-margin: 10px;
-`
+  text-align: right;
+  padding-top: 10px;
+  padding: 25px;
+  margin: 10px;
+`;
 
 const OnBoard = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <Container>
+      <Span
+        style={{
+          color: "#196052",
+        }}
+      >
+        <Exit onClick={() => navigate("/login")}>X</Exit>
+      </Span>
+      <PandaGreeting>
         <Span
           style={{
-            color: "#196052",
-          }}
-        >
-          <Exit onClick={() => navigate("/login")}>
-          X
-          </Exit>
-        </Span>
-      <PandaGreeting>
-          <Span style={{
             fontSize: 25,
             // position: absolute,
             // top: 50,
             // left: 50,
             // transform: translate(-50, -50),
-          }}>"</Span>A bond between friends is impossible to break, except
-          when deciding where to eat.
-          <Span style={{
-            fontSize: 25
-          }}>"</Span>
-          <Span>
-            {/* <p 
+          }}
+        >
+          "
+        </Span>
+        A bond between friends is impossible to break, except when deciding
+        where to eat.
+        <Span
+          style={{
+            fontSize: 25,
+          }}
+        >
+          "
+        </Span>
+        <Span>
+          {/* <p 
           style={{
             fontSize: 20,
           }}
           >- Hungry Panda</p> */}
-           {/* <img
+          {/* <img
           src={PandaSpeechBubble}
           alt="panda speech bubble"
           style={{
             width: "100%",
           }} */}
-        {/* /> */}
-          </Span>
+          {/* /> */}
+        </Span>
       </PandaGreeting>
-     
+
       <div id="container">
         <PandaTalk>
-        <img
-          src={talkingPanda}
-          alt="panda gif"
-          style={{
-            width: 200,
-          }}
-        /></PandaTalk>
+          <img
+            src={talkingPanda}
+            alt="panda gif"
+            style={{
+              width: 200,
+            }}
+          />
+        </PandaTalk>
       </div>
       <i
         style={{
           color: "#196052",
         }}
       >
-        <Blurb
-        >
+        <Blurb>
           Let Find Dining help with the emotional labor of deciding what's for
           dinner. Register & invite a friend to choose a restaurant.
         </Blurb>
       </i>
+      <div className="text-center">
       <StyledButton onClick={() => navigate("/login")}>
         I'm Hungry!
       </StyledButton>
+      </div>
     </Container>
   );
 };
