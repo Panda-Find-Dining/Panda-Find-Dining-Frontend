f68713de4dad1d1215f088672043b0449f0c4a9e
Paul Reece <paulreece2@gmail.com>
2022-05-03 20:58:01 -0400

    netlify function

--- find-dining/src/netlify/functions/pictures.ts
+++ find-dining/src/netlify/functions/pictures.ts
@@ -4,10 +4,9 @@
   import axios from "axios"
 
 const handler = async (event) => {
-  const {lat, long} = event.queryStringParameters
+  // const {restaurant} = event.queryStringParameters
 
-  const API_SECRET = process.env.API_SECRET 
-  const url = `http://api.weatherstack.com/current?access_key=${API_SECRET}&query=${lat},${long}&forcast_days=4`
+  const url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=Aap_uEA9-OWU73s-8F2SV1fDj4VzEw0NWTGlhXrUyaQ-gPyf1kinnApe8WVakQbsf-ZPtQ0yica1yvUVO6ZnjVH1eqpTYjjpH4FiKW0JMcXjlNI3_nChIDxRCXIu7-3_EjQTmSLWWNPyij3iAzAmfm1x5Z9FqpCmTVyFj2KDphfhPwRKZUHL&key=${process.env.GOOGLE_API_KEY}`
 
   try {
     const { data } = await axios.get(url)
