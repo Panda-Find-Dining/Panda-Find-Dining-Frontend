4120a8449c17b2bbf91b36cb29f0bc17684d9111
Paul Reece <paulreece2@gmail.com>
2022-05-03 21:34:58 -0400

    netlify function

--- find-dining/netlify/functions/pictures.js
+++ find-dining/netlify/functions/pictures.js
@@ -6,7 +6,7 @@ exports.handler = async function (event, context) {
   try {
     // const { id } = event.queryStringParameters;
     const response = await axios.get(
-      "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference="
+      `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=Aap_uEAkQC_LMHuyp10G7d0dkdEiVJ4-6jsIUKS99EHS5kn5j2KumG4kOqAGEjvFMIbi0QRf-ugPBInEEKgzylE5U_oUf0oDtRlU_8LLMKzYHT8cOD_bncZgEgtWynTyDeeicZ57UvWhlTP8n_7XUxOSNq7yhjR9z03j32avnpuhUkOULocC&key=${process.env.GOOGLE_API_KEY}`
     );
     return {
       statusCode: 200,
